e4312f558c0a5a17a64155cf1f6385359c4bfaf1
touch <touchgsx@gmail.com>
2016-09-08 18:22:57 +0800

    Initial Commit

---
+++ UIKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIKit
+//
+//  Created by Touch on 9/8/16.
+//  Copyright © 2016 ___Touch___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UIKit
+//
+//  Created by Touch on 9/8/16.
+//  Copyright © 2016 ___Touch___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
